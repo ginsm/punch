9c9ce5af76f77590b56d5360634fda8d05fa9628
ginsm <matt@mgin.me>
2020-03-21 06:15:37 -0400

    Ensure that 'state.json' exists before issuing any commands

--- src/controller/commands.py
+++ src/controller/commands.py
@@ -1,6 +1,7 @@
 # SECTION IMPORTS - External and internal imports.
 # =====================================================
 # Internal
+import model.db as db
 import controller.command as command
 import command.punch as punch
 import command.job as job
@@ -10,6 +11,12 @@ import command.rm as rm
 # SECTION  HANDLER - The command handler.
 # =====================================================
 def handle(command, argument):
+  # Ensure the state exists
+  state = db.get_state()
+  if state is False:
+    # return view.createFirstJob()
+    return False
+
   if command is None:
     command = 'punch'
     
