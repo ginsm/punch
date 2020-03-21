e2e618000b7749985d304b89643711dec610f923
ginsm <matt@mgin.me>
2020-03-21 07:14:47 -0400

    Prevent directory paths in job name

--- src/controller/command/job.py
+++ src/controller/command/job.py
@@ -2,6 +2,7 @@
 # =====================================================
 # Internal
 import model.db as db
+import re
 
 
 # SECTION HANDLER - Job selection handlers.
@@ -12,6 +13,10 @@ def handler(command, argument):
     # return view.currentJob()
     return
 
+  if re.search('\/', argument) is not None:
+    # return view.invalidJobName(argument)
+    return False
+
   db.set_state({'job': argument})
 
   if not db.exists(argument):
@@ -19,4 +24,4 @@ def handler(command, argument):
     schema['name'] = argument
     db.write(schema, argument)
   
-  # return view.newSelectedJob()
+  # return view.newSelectedJob(argument)
