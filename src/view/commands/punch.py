e02715c33cec76d7fe3c9f77c7141ce6da34025a
ginsm <matt@mgin.me>
2020-03-21 09:30:19 -0400

    Adjust punch-in/punch-out messages

--- src/view/commands/punch.py
+++ src/view/commands/punch.py
@@ -3,12 +3,12 @@
 def punched_out(database, hours):
   name = database['name']
   hours = round(hours, 2)
-  print('Punching out at %s hours for "%s".' % (hours, name))
+  print('Punching out with a total of %s hours for "%s".' % (hours, name))
   return True
 
 
 def punched_in(database):
   name = database['name']
   hours = round(database['totalHours'], 2)
-  print('Punching in with %s hours for "%s".' % (hours, name))
+  print('Punching in with a total of %s hours for "%s".' % (hours, name))
   return True
