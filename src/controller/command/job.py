a801e7ed40852c8b686beed9d981b14868fdaeba
ginsm <matt@mgin.me>
2020-03-21 06:34:41 -0400

    Add job command — handles both None and String argument

--- src/controller/command/job.py
+++ src/controller/command/job.py
@@ -1,4 +1,23 @@
+# SECTION IMPORTS - External and internal imports.
+# =====================================================
+# Internal
+import model.db as db
+
+
 # SECTION HANDLER - Job selection handlers.
 # =====================================================
 def handler(command, argument):
-  pass
+  if argument is None:
+    print('Current Job: %s' % str(db.get_state()['job']))
+    # return view.currentJob()
+    return
+
+  job = argument
+  db.set_state({'job': job})
+
+  if not db.exists(job):
+    schema = db.get_state('schema')
+    schema['name'] = job
+    db.write(schema, job)
+  
+  # return view.newSelectedJob()
